0ef0d18e6b5713cdf41e2698f18125adfa7c31ea
Harshal <harshal@vhyom.com>
2023-05-16 14:00:32 +0530

    Get SubscriptionDetails  by uuid

--- src/main/java/com/vhyom/saas/controller/SubscriptionDetailsController.java
+++ src/main/java/com/vhyom/saas/controller/SubscriptionDetailsController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Objects;
 
 @RequestMapping("api/v1/subscriptionDetails")
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class SubscriptionDetailsController {
 
